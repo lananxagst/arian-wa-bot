9afb52333921058d93ed74e14966ae533b490476
lananxagst <67673253+lananxagst@users.noreply.github.com>
2025-03-19 12:03:02 +0800

    update package.json

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
 const client = new Client({
-  authStrategy: new LocalAuth(), // Menyimpan sesi login agar tidak perlu scan ulang
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
 });
 
 client.on("qr", (qr) => {
